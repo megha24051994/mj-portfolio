009f5ec43f74284dd972a883fda1a0c950a70852
Megha Jayakumar <megha24051994@gmail.com>
2023-08-03 23:38:08 +0200

    wip

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Megha Jayakumar',
     title: "Web/App Developer",
-    desciption:"<Hello World />, I'm Megha Jayakumar!",
+    desciption:"<HelloWorld />, I'm Megha Jayakumar!",
     para1: "A web and app developer from Germany, armed with a master's in Computer Science. Go ahead and scroll to discover more about me!",
     resumePdf: resume
 }
